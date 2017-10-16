c98cc624b4a6219541f1d13cc79d5b61e31d5783
Amélie Meurer <ameliemeurer@MacBook-Air-dAmelie.local>
2017-10-16 16:34:20 +0200

    Ajout de la fonction my csv reader

---
+++ R/Fonction my csv reader.R
@@ -0,0 +1,13 @@
+#' Title
+#'
+#' @param folder Put the name of a folder
+#'
+#' @return Takes all the csv from a folder Import them and put them in a list Returns the list
+#' @export
+#'
+#' @examples
+my_csv_reader <- function(folder){
+  l <- list.files(path = folder, pattern = ".*csv$", full.names = TRUE)
+  result <- lapply(l,read.csv2)
+  return(result)
+}
