b44db886081a53dd35f4a015b1a2e9540fb5325d
Amélie Meurer <amelie.meurer@hec.edu>
2017-10-17 17:50:31 +0200

    Ajout Vignette

--- R/draw_a_name.R
+++ R/draw_a_name.R
@@ -3,7 +3,7 @@
 #' @param the_name : give a name
 #' @param the_sex : give the sex
 #'
-#' @return which can plot the popularity of babynames (using prenoms) in times use the_name and the_sex as arguments.
+#' @return can plot the popularity of babynames (using prenoms) in times use the_name and the_sex as arguments.
 #' @export
 #' @import prenoms ggplot2 dplyr assertthat
 #'
@@ -11,6 +11,7 @@
 draw_a_name <- function(the_name = n, the_sex= s) {
   assert_that(is.character(the_name))
   assert_that(is.character(the_sex))
+  assert_that(the_sex == "F" |the_sex=="M")
   prenoms_by_year <- prenoms::prenoms %>%  filter (name == the_name, sex == the_sex) %>% group_by(year)%>% summarise(n=sum(n))
   ggplot(data=prenoms_by_year,
          aes( x = year, y = n )) +
